ed939d0998efaa8ef2eb732408ed8ac9ea2a852e
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-01-01 19:39:44 +0700

    feat(ai): add authGuard on requestAiChat

--- src/api/controller/ai/requestAIChat.ts
+++ src/api/controller/ai/requestAIChat.ts
@@ -10,40 +10,30 @@ const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" })
 
 export default createElysia()
   .use(aiModel)
-  // .use(authGuard)
-  // .post("/", async ({ body, user }) => {
-  .post("/", async ({ body }) => {
+  .use(authGuard)
+  .post("/", async ({ body, user }) => {
     const { text } = body;
     const result = await model.generateContent(text);
-    /**
-     * TODO: No auth guard for a while until the front end is ready
-     */
     if (result?.response?.candidates) {
+      const ai = await prismaClient.AIChat.create({
+        data: {
+          userId: user.id,
+          chatTitle: text,
+        }
+      });
+      await prismaClient.AIChatMessage.create({
+        data: {
+          msg: result?.response?.candidates[0]?.content?.parts?.map((part: any) => part.text).join(" "),
+          role: result?.response?.candidates[0]?.content?.role,
+          createdAt: new Date(),
+          aIChatId: ai?.id,
+        },
+      });
       return {
         status: 200,
-        data: result.response.candidates[0]?.content?.parts?.map((part: any) => part.text).join(" ")
+        data: result?.response?.candidates[0]?.content?.parts?.map((part: any) => part.text).join(" ")
       };
     }
-    // if (result?.response?.candidates) {
-    //   const ai = await prismaClient.AIChat.create({
-    //     data: {
-    //       userId: user.id,
-    //       chatTitle: text,
-    //     }
-    //   });
-    //   await prismaClient.AIChatMessage.create({
-    //     data: {
-    //       msg: result?.response?.candidates[0]?.content?.parts?.map((part: any) => part.text).join(" "),
-    //       role: result?.response?.candidates[0]?.content?.role,
-    //       createdAt: new Date(),
-    //       aIChatId: ai?.id,
-    //     },
-    //   });
-    //   return {
-    //     status: 200,
-    //     data: result?.response?.candidates[0]?.content?.parts?.map((part: any) => part.text).join(" ")
-    //   };
-    // }
     return new InternalServerErrorException("Failed to generate content");
   }, {
     body: "ai.model",
