3646cedbfc43acdb3eab45244cef40b8c9494699
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-10-12 22:11:14 +0700

    feat(tools): add anime random pics

--- src/api/controller/tools/getAnime.ts
+++ src/api/controller/tools/getAnime.ts
@@ -3,40 +3,71 @@ import { createElysia } from "@/libs/elysia";
 export default createElysia()
   .get("/anime/waifu", async () => {
     const res = await fetch("https://api.waifu.pics/sfw/waifu");
-    const data = await res.json();
-    return {
+    const { url } = await res.json();
+    const imgRes = await fetch(url);
+    const contentType = imgRes.headers.get("content-type") ?? "image/jpeg";
+    return new Response(imgRes.body, {
       status: 200,
-      message: "Success",
-      result: data.url,
-    };
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
   }, {
-    detail: {
-      tags: ["Anime"],
-    }
+    detail: { tags: ["Anime"] }
   })
   .get("/anime/neko", async () => {
     const res = await fetch("https://api.waifu.pics/sfw/neko");
-    const data = await res.json();
-    return {
+    const { url } = await res.json();
+    const imgRes = await fetch(url);
+    const contentType = imgRes.headers.get("content-type") ?? "image/jpeg";
+    return new Response(imgRes.body, {
       status: 200,
-      message: "Success",
-      result: data.url,
-    };
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
   }, {
-    detail: {
-      tags: ["Anime"],
-    }
+    detail: { tags: ["Anime"] }
   })
   .get("/anime/cringe", async () => {
     const res = await fetch("https://api.waifu.pics/sfw/cringe");
-    const data = await res.json();
-    return {
+    const { url } = await res.json();
+    const imgRes = await fetch(url);
+    const contentType = imgRes.headers.get("content-type") ?? "image/jpeg";
+    return new Response(imgRes.body, {
       status: 200,
-      message: "Success",
-      result: data.url,
-    };
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
   }, {
-    detail: {
-      tags: ["Anime"],
-    }
+    detail: { tags: ["Anime"] }
+  })
+  .get("/anime/blush", async () => {
+    const res = await fetch("https://api.waifu.pics/sfw/blush");
+    const { url } = await res.json();
+    const imgRes = await fetch(url);
+    const contentType = imgRes.headers.get("content-type") ?? "image/jpeg";
+    return new Response(imgRes.body, {
+      status: 200,
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
+  }, {
+    detail: { tags: ["Anime"] }
+  })
+  .get("/anime/dance", async () => {
+    const res = await fetch("https://api.waifu.pics/sfw/dance");
+    const { url } = await res.json();
+    const imgRes = await fetch(url);
+    const contentType = imgRes.headers.get("content-type") ?? "image/jpeg";
+    return new Response(imgRes.body, {
+      status: 200,
+      headers: {
+        "Content-Type": contentType,
+      },
+    });
+  }, {
+    detail: { tags: ["Anime"] }
   })
