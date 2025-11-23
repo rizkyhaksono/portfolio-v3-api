17b39c1dcbbe47392b05cb6dd2ee86918c9dbd71
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-11-24 00:33:44 +0700

    feat(assets): remove clodinary and add types for minio

--- src/api/controller/asset/index.ts
+++ src/api/controller/asset/index.ts
@@ -1,9 +1,7 @@
-import cloudinaryUpload from "./cloudinaryUpload";
 import minioUpload from "./minioUpload";
 import minioDownload from "./minioDownload";
 
 export {
-  cloudinaryUpload,
   minioUpload,
   minioDownload
 }
--- src/models/cloudinary.model.ts
+++
@@ -1,9 +0,0 @@
-import Elysia, { t } from "elysia";
-
-const cloudinaryModel = new Elysia().model({
-  "cloudinary.model": t.Object({
-    image: t.String(),
-  }),
-});
-
-export default cloudinaryModel;
