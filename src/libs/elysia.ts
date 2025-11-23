3ad5be55ea582f3a93a2c677db1c6c8ee19fa391
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-11-24 00:35:34 +0700

    feat(libs): add instrumentation and openelemetry elysia

--- src/libs/elysia.ts
+++ src/libs/elysia.ts
@@ -3,11 +3,8 @@ import logger from "./logger";
 import { error } from "@/utils/errorHandler";
 import { env } from "./env";
 
-const baseElysia = <
-  const BasePath extends string = "",
-  const Scoped extends boolean = false
->(
-  config?: ElysiaConfig<BasePath, Scoped>
+const baseElysia = <const BasePath extends string = "">(
+  config?: ElysiaConfig<BasePath>
 ) => new Elysia(config).use(env).use(logger).use(error);
 
 const createElysia = (config?: Parameters<typeof baseElysia>[0]) =>
