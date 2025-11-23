19c7870324c2beafa8b3ae37ba2310615e2cdb0b
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-11-24 00:41:32 +0700

    feat(education): implement guards, and pagination

--- src/api/controller/education/getEducationById.ts
+++ src/api/controller/education/getEducationById.ts
@@ -1,17 +1,42 @@
 import { createElysia } from "@/libs/elysia";
 import { prismaClient } from "@/libs/prismaDatabase";
+import educationModel from "@/models/education.model";
 
 export default createElysia()
-  .get("/:id", async ({ params: { id } }) => {
+  .use(educationModel)
+  .get("/:id", async ({
+    params: { id }
+  }: {
+    params: { id: string };
+  }) => {
     const education = await prismaClient.education.findUnique({
-      where: { id: parseInt(id) },
+      where: { id },
     })
     return {
       status: 200,
       data: education
     }
   }, {
+    body: "education.model",
     detail: {
-      tags: ["Education"]
+      tags: ["Education"],
+      summary: "Get an education record by ID",
+      description: "Retrieves a specific education record from the database using its ID.",
+      response: {
+        200: {
+          description: "Education record retrieved successfully",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  status: { type: "number" },
+                  data: { $ref: "#/components/schemas/Education" }
+                }
+              }
+            }
+          }
+        }
+      }
     }
   })
