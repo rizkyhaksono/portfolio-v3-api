c50ff33a51586b14859efa9a2c9b92931a104aa4
rizkyhaksono <mrizkyhaksono@gmail.com>
2025-11-24 00:44:55 +0700

    feat(work): implement guards, tags, and add types

--- src/api/controller/work/updateWork.ts
+++ src/api/controller/work/updateWork.ts
@@ -2,13 +2,15 @@ import { createElysia } from "@/libs/elysia";
 import { prismaClient } from "@/libs/prismaDatabase";
 import { authGuard } from "@/libs/authGuard";
 import workModel from "@/models/work.model";
+import { adminGuard } from "@/libs/roleGuards";
 
 export default createElysia()
   .use(workModel)
   .use(authGuard)
+  .use(adminGuard)
   .patch("/:id", async ({ body, params: { id } }) => {
     await prismaClient.work.update({
-      where: { id: parseInt(id) },
+      where: { id },
       data: body
     })
     return {
@@ -18,6 +20,8 @@ export default createElysia()
   }, {
     body: "work.model",
     detail: {
-      tags: ["Work"]
+      tags: ["Work"],
+      summary: "Update Work",
+      description: "Update an existing work entry by its ID"
     }
   })
